8f94593e659e7a86ca92e2307b76be330687de04
fezzy <fezzy@fezzy-VirtualBox.(none)>
2013-09-07 10:09:50 -0400

    'Rework for HALO part 1 of 2.'

--- src/com/android/settings/cyanogenmod/MiscSettings.java
+++ src/com/android/settings/cyanogenmod/MiscSettings.java
@@ -1,81 +1,110 @@
 /*
- * Copyright (C) 2013 BeanStalk
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright (C) 2012 ParanoidAndroid Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package com.android.settings.cyanogenmod;
 
 import android.app.ActivityManager;
-import android.content.ContentResolver;
-import android.database.ContentObserver;
+import android.app.AlertDialog;
+import android.app.INotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.os.Bundle;
-import android.os.Handler;
-import android.preference.CheckBoxPreference;
 import android.os.ServiceManager;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
-import android.app.INotificationManager;
+import android.provider.Settings.SettingNotFoundException;
 
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R; 
+import com.android.settings.SettingsPreferenceFragment; 
+import com.android.settings.Utils;
+import com.android.settings.util.Helpers;
 
 public class MiscSettings extends SettingsPreferenceFragment
-                        implements Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_ENABLED = "halo_enabled"; 
+    private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_REVERSED = "halo_reversed";
-
-    private ContentResolver mContentResolver;
+    private static final String KEY_HALO_PAUSE = "halo_pause";
+    private static final String KEY_HALO_SIZE = "halo_size";
+    
     private ListPreference mHaloState;
-    private CheckBoxPreference mHaloEnabled; 
+    private ListPreference mHaloSize;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
+    private CheckBoxPreference mHaloPause;
+    private SwitchPreference mHaloEnabled;
+    
+    
+    private Context mContext;
     private INotificationManager mNotificationManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getActivity();
 
         addPreferencesFromResource(R.xml.halo);
-
         PreferenceScreen prefSet = getPreferenceScreen();
+        mContext = getActivity();
 
-	mNotificationManager = INotificationManager.Stub.asInterface(
+        mNotificationManager = INotificationManager.Stub.asInterface(
                 ServiceManager.getService(Context.NOTIFICATION_SERVICE));
 
-        mHaloEnabled = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_ENABLED);
-        mHaloEnabled.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_ENABLED, 0) == 1);
+	mHaloEnabled = (SwitchPreference) findPreference(KEY_HALO_ENABLED);
+        mHaloEnabled.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.HALO_ENABLED,
+                0) == 1);
+        mHaloEnabled.setOnPreferenceChangeListener(this); 
 
-        mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
+        mHaloState = (ListPreference) findPreference(KEY_HALO_STATE);
         mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
         mHaloState.setOnPreferenceChangeListener(this);
 
-        mHaloHide = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_HIDE);
+        mHaloHide = (CheckBoxPreference) findPreference(KEY_HALO_HIDE);
         mHaloHide.setChecked(Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_HIDE, 0) == 1);
 
-        mHaloReversed = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_REVERSED);
+        mHaloReversed = (CheckBoxPreference) findPreference(KEY_HALO_REVERSED);
         mHaloReversed.setChecked(Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_REVERSED, 1) == 1);
+
+	int isLowRAM = (ActivityManager.isLargeRAM()) ? 0 : 1;
+        mHaloPause = (CheckBoxPreference) findPreference(KEY_HALO_PAUSE);
+        mHaloPause.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_PAUSE, isLowRAM) == 1);
+
+	mHaloSize = (ListPreference) findPreference(KEY_HALO_SIZE);
+        try {
+            float haloSize = Settings.System.getFloat(mContext.getContentResolver(),
+                    Settings.System.HALO_SIZE, 1.0f);
+            mHaloSize.setValue(String.valueOf(haloSize));
+        } catch(Exception ex) {
+            // So what
+        }
+        mHaloSize.setOnPreferenceChangeListener(this);
     }
 
     private boolean isHaloPolicyBlack() {
@@ -88,44 +117,50 @@ public class MiscSettings extends SettingsPreferenceFragment
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mHaloState) {
-            boolean state = Integer.valueOf((String) objValue) == 1;
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mHaloHide) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_HIDE, mHaloHide.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloEnabled) {
+             Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_ENABLED, mHaloEnabled.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloReversed) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_REVERSED, mHaloReversed.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloPause) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_PAUSE, mHaloPause.isChecked()
+                    ? 1 : 0);
+            Helpers.restartSystemUI();
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object Value) {
+        if (preference == mHaloEnabled) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.HALO_ENABLED,
+                    (Boolean) Value ? 1 : 0);
+            return true;
+        } else if (preference == mHaloState) {
+            boolean state = Integer.valueOf((String) Value) == 1;
             try {
                 mNotificationManager.setHaloPolicyBlack(state);
             } catch (android.os.RemoteException ex) {
                 // System dead
             }
             return true;
+        } else if (preference == mHaloSize) {
+            float haloSize = Float.valueOf((String) Value);
+            int index = mHaloSize.findIndexOfValue((String) Value);
+            Settings.System.putFloat(getActivity().getContentResolver(),
+                    Settings.System.HALO_SIZE, haloSize);
+            mHaloSize.setSummary(mHaloSize.getEntries()[index]);
+            return true;
         }
         return false;
     }
-
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        boolean value;
-        if (preference == mHaloEnabled) {
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_ENABLED, mHaloEnabled.isChecked()
-                    ? 1 : 0);
-	} else if (preference == mHaloHide) {
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_HIDE,
-                    mHaloHide.isChecked() ? 1 : 0);  
-        } else if (preference == mHaloReversed) {
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_REVERSED,
-                    mHaloReversed.isChecked() ? 1 : 0);
-        } else {
-            // If we didn't handle it, let preferences handle it.
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
-        }
-        return true;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-}
-
+ }
